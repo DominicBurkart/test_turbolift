b4ee74075947fcd325bbd45281c5c7bc0e06b429
Dominic Burkart <1351120+DominicBurkart@users.noreply.github.com>
2021-06-14 07:31:06 +0200

    setup turbolift test program

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+/// Tests that [turbolift](https://dominic.computer/turbolift) when built from cargo
+/// instead of from source. Like all libs using proc_macro2's unstable features,
+/// at writing this project needs to be run with semver exemption, like
+/// `RUSTFLAGS='--cfg procmacro2_semver_exempt' cargo run`.
+use std::time::{SystemTime, Instant};
+use std::error::Error;
+
+// Instantiate cluster interface.
+lazy_static! {
+    static ref K8S: Mutex<K8s> = Mutex::new(
+        K8s::with_deploy_function_and_max_replicas(
+            Box::new(load_container_into_kind),
+            2
+        )
+    );
+}
+
+/// Tells the cluster to make images accessible by sending them
+/// to kind (https://kind.sigs.k8s.io/).
+fn load_container_into_kind(tag: &str) -> anyhow::Result<&str> {
+    std::process::Command::new("kind")
+        .args(
+            format!("load docker-image {}", tag)
+                .as_str()
+                .split(' ')
+        )
+        .status()?;
+    Ok(tag)
+}
+
+/// function to distribute. Note this non-async call is turned into
+/// an async call.
+#[on(K8S)]
+fn current_time() -> String {
+    "Hello, world!".to_string()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("time in cluster: {}", current_time().await);
+    Ok(())
+}
